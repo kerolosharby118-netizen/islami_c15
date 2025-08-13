1f382290712ac18741d5fd163899d610751e8cb5
Harby <kerolosharby118@gmail.com>
2025-08-13 18:57:10 +0300

    ahadeth_tab details

--- lib/ui/screans/home/tabs/ahadeth/ahadeth_tab.dart
+++ lib/ui/screans/home/tabs/ahadeth/ahadeth_tab.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+class Hadeth{
+  String title;
+  String content;
+  Hadeth(this.title,this.content);
+}
+class AhadethTab extends StatefulWidget {
 
-class AhadethTab extends StatelessWidget {
   const AhadethTab({super.key});
 
   @override
+  State<AhadethTab> createState() => _AhadethTabState();
+}
+
+class _AhadethTabState extends State<AhadethTab> {
+  List <Hadeth> ahadeth =[];
+
+  @override
+  void initState(){
+    super.initState();
+    readAhadethFile();
+  }
+
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  void readAhadethFile()async{
+    String ahadethFile = await rootBundle.loadString("assets/files/ahadeth.txt,");
   }
 }
